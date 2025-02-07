333baa79f61ba6c5ded385b65e6cf481ec84f861
Blake Kaufman <blakekaufman9@gmail.com>
2025-02-07 14:29:40 -0500

    adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': 'off',
+    'dot-notation': 'off',
+    curly: 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'no-undef': 2,
+    'no-unused-vars': 0,
+    'no-unreachable': 'off',
+    quotes: 'off',
+  },
+};
