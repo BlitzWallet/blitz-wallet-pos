acc0ac73e3e49fd63ecadbfa411c91ab53e13f69
Blake Kaufman <blakekaufman9@gmail.com>
2025-02-07 12:31:57 -0500

    redirect hook for pages past initialization

---
+++ src/hooks/redirectWhenNoAccount.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useGlobalContext } from "../contexts/posContext";
+import { useNavigate, useParams } from "react-router-dom";
+
+const NoAccountRedirect = (posPageRoute) => {
+  const navigate = useNavigate();
+  const { currentUserSession } = useGlobalContext();
+  useEffect(() => {
+    console.log(currentUserSession, "CURRENT USER SESSION IN REDIRECT");
+    if (!currentUserSession.account && !currentUserSession.bitcoinPrice) {
+      navigate(posPageRoute, { replace: true });
+    }
+  }, []);
+};
+
+export default NoAccountRedirect;
