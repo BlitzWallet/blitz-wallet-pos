96be366a4e7fe86f919ceeade07ece30a10b6837
Blake Kaufman <blakekaufman9@gmail.com>
2025-02-10 15:29:53 -0500

    use server proxy

--- src/functions/getUserFromFirebase.js
+++ src/functions/getUserFromFirebase.js
@@ -1,5 +1,5 @@
 export async function setupSession(wantedName) {
-  const response = await fetch(`${getBackendURL()}`, {
+  const response = await fetch("/getUser", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -8,23 +8,12 @@ export async function setupSession(wantedName) {
       storeName: wantedName,
     }),
   });
-
   const data = await response.json();
 
   if (response.status !== 200) throw new Error(data?.error || "BAD REQUEST");
-  return { posData: data.posData, bitcoinPrice: data.bitcoinData };
-}
-function getBackendURL() {
-  if (process.env.REACT_APP_FUNCTIONS_ENVIRONMENT === "testnet") {
-    console.log("Running in development mode");
-    return process.env.REACT_APP_NETLIFY_BACKEND_DEV;
-  } else if (process.env.REACT_APP_FUNCTIONS_ENVIRONMENT === "liquid") {
-    console.log("Running in production mode");
-    return process.env.REACT_APP_NETLIFY_BACKEND_PROD;
-  } else {
-    console.log(
-      "Unknown environment:",
-      process.env.REACT_APP_FUNCTIONS_ENVIRONMENT
-    );
-  }
+
+  return {
+    posData: data?.data?.posData?.posSettings,
+    bitcoinPrice: data?.data?.bitcoinData,
+  };
 }
