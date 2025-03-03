73fd2cb98d67c06bdba01cb621aeeebf24c79ac0
Blake Kaufman <blakekaufman9@gmail.com>
2025-03-03 14:46:39 -0500

    added settings icon to navbar

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,8 +1,12 @@
+import { useLocation } from "react-router-dom";
+import { useSettingsDisplay } from "../../contexts/settingsDisplay";
 import getCurrentUser from "../../hooks/getCurrnetUser";
 import "./style.css";
 
 export default function PosNavbar({ backFunction }) {
   const User = getCurrentUser();
+  const { setDisplaySettings } = useSettingsDisplay();
+  const location = useLocation();
 
   return (
     <div className="POS-navbar">
@@ -15,6 +19,16 @@ export default function PosNavbar({ backFunction }) {
         src="/assets/icons/arrowLeft.png"
       />
       <h2 className="POS-name">{User}</h2>
+      {location.pathname.split("/").length == 2 && (
+        <img
+          onClick={() => {
+            setDisplaySettings(true);
+          }}
+          alt="Back arrow"
+          className="nav-settings"
+          src="/assets/icons/settings.png"
+        />
+      )}
     </div>
   );
 }
