a5556660d84b4eaa7a800544cf26f9e37fac8eb2
Blake Kaufman <blakekaufman9@gmail.com>
2026-04-13 20:47:33 -0400

    adding more translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import en from "./locales/en/translation.json";
+import enTranslation from "./locales/en/translation.json";
+import esTranslation from "./locales/es/translation.json";
+import itTranslation from "./locales/it/translation.json";
+import ptBrTranslation from "./locales/pt-BR/translation.json";
+import germanTranslation from "./locales/de-DE/translation.json";
+import frenchTranslation from "./locales/fr/translation.json";
 
 i18n
   .use(LanguageDetector)
@@ -9,8 +14,17 @@ i18n
   .init({
     initImmediate: false,
     fallbackLng: "en",
+    supportedLngs: ["en", "es", "it", "pt", "pt-BR", "de", "de-DE", "fr"],
+    // load: "currentOnly",
     resources: {
-      en: { translation: en },
+      en: { translation: enTranslation },
+      es: { translation: esTranslation },
+      it: { translation: itTranslation },
+      "pt-BR": { translation: ptBrTranslation },
+      pt: { translation: ptBrTranslation },
+      "de-DE": { translation: germanTranslation },
+      de: { translation: germanTranslation },
+      fr: { translation: frenchTranslation },
     },
     interpolation: {
       escapeValue: false,
